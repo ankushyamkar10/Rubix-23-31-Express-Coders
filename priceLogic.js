ee54d24e23f58f8d4c01d1d663bb161e35dc93d0
LakshBulchandani <94983429+LakshBulchandani@users.noreply.github.com>
2023-01-20 01:44:02 +0530

    Add files via upload

--- priceLogic.js
+++ priceLogic.js
@@ -1,10 +1,8 @@
-function price(item_name, item_type, max_expiry, base_price, total_qty, expiry_days, distance)
+function price(item_name, item_type, max_expiry, base_price, total_qty, expiry_days)
 {
     var final_price = base_price, factor = 0;
-    if(distance > 10)
-        final_price = base_price + base_price * (distance - 10) * 0.01;
     if(total_qty > 100)
-    factor = (total_qty - 100) / 10000;
+    factor = total_qty / 10000;
     factor += (max_expiry - expiry_days) / (max_expiry);
     if(factor > 0.7)
         factor = 0.7;
@@ -12,4 +10,4 @@ function price(item_name, item_type, max_expiry, base_price, total_qty, expiry_d
     console.log(Math.round(final_price), factor);
 }
 
-price('Amul Butter 100g','Dairy', 270, 55, 120, 200, 12);
+//price('Amul Butter 100g','Dairy', 270, 55, 120, 200);
