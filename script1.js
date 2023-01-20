e250be8240496fb00d4cada26a7e3e2ef36935ed
mozilla01 <moizrocks12345@gmail.com>
2023-01-20 09:55:57 +0530

    minor updates

--- script1.js
+++ script1.js
@@ -44,13 +44,11 @@ document.getElementById("btn").addEventListener("click", function (event) {
 });
 
 //Map integration
-
-console.log("hello");
-
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
+console.log("hello");
 let map, infoWindow;
 
 function initMap() {
